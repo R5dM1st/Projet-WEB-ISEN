2178c0ed0b5ea4b65d7a47b099e13d4750307f34
R5dM1st <emile.duplais@isen-ouest.yncrea.fr>
2025-06-23 13:51:19 +0200

    ye

---
+++ data/sql/init.sql
@@ -0,0 +1,40 @@
+DROP TABLE IF EXISTS Position CASCADE;
+DROP TABLE IF EXISTS Bateau CASCADE;
+DROP TABLE IF EXISTS TYPE_Bateau CASCADE;
+DROP TABLE IF EXISTS Utilisateur CASCADE;
+
+-- Table TYPE_Bateau
+CREATE TABLE TYPE_Bateau (
+    Vessel_type INT PRIMARY KEY,
+    Labelle VARCHAR(50)
+);
+
+-- Table Bateau
+CREATE TABLE Bateau (
+    MMSI INT PRIMARY KEY,
+    Nom VARCHAR(50),
+    Longueur FLOAT,
+    Largeur FLOAT,
+    Vessel_type INT,
+    FOREIGN KEY (Vessel_type) REFERENCES TYPE_Bateau(Vessel_type)
+);
+
+-- Table Position
+CREATE TABLE Position (
+    id SERIAL PRIMARY KEY,  -- Correction ici
+    Date_Heure TIMESTAMP,   -- Correction du type
+    Latitude FLOAT,
+    Longitude FLOAT,
+    Vitesse FLOAT,
+    CAP FLOAT,
+    MMSI INT,
+    FOREIGN KEY (MMSI) REFERENCES Bateau(MMSI)
+);
+
+-- Table Utilisateur
+CREATE TABLE Utilisateur (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    mot_de_passe VARCHAR(255) NOT NULL
+);
